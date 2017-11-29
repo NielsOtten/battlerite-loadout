eba98191f5e1bffbedd58b5b82520e38a5cf3869
Niels <niels@burst-digital.com>
2017-11-29 12:56:55 +0100

    Remove unused files

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from '../../scenes/Home';
-import './App.css';
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path='/' component={Home} />
   </Switch>
 );
 
